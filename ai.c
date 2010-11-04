cbdc1abe68ebac831e138769ec206e922420a218
Mathias Rav <rav@cs.au.dk>
2010-11-04 13:45:51 +0100

    Implement act_dualcheck

--- ai.c
+++ ai.c
@@ -22,32 +22,62 @@ static int nexttileidx(Minefield *f) {
 	return nexttileidx_ = 0;
 }
 
-typedef bool (*neighbourcount_cb)(Tile *tile, void *payload);
+typedef bool (*neighbourcount_cb)(Minefield *f, Tile *tile, Coordinate *coord, void *payload);
 
-static int neighbourcount(Minefield *f, int tile, neighbourcount_cb cb, void *cbpayload) {
-	Coordinate *neighbours[f->maxneighbours];
-	neighbourhood(f, tile, (Coordinate **) neighbours);
+static int neighbourcount(Minefield *f, Coordinate **neighbours, neighbourcount_cb cb, void *cbpayload) {
 	int i = 0;
 	int matches = 0;
 	while (neighbours[i] != NULL) {
 		Coordinate *coord = neighbours[i++];
 		Tile *tile = &f->tiles[coordstoidx(f, coord)];
-		if ((*cb)(tile, cbpayload)) {
+		if ((*cb)(f, tile, coord, cbpayload)) {
 			++matches;
 		}
 	}
 	return matches;
 }
 
-#define CB(fun) static bool fun(Tile *tile, void *payload)
+static void neighbourfilter(Minefield *f, Coordinate **c, neighbourcount_cb cb, void *cbpayload) {
+	Coordinate **dest = c;
+	int i = 0;
+	while (c[i] != NULL) {
+		Coordinate *coord = c[i++];
+		Tile *tile = &f->tiles[coordstoidx(f, coord)];
+		if ((*cb)(f, tile, coord, cbpayload)) {
+			if (&c[i] != dest) {
+				c[i] = *dest;
+			}
+			++dest;
+		}
+	}
+	*dest = NULL;
+}
+
+#define CB(fun) static bool fun(Minefield *f, Tile *tile, Coordinate *coords, void *payload)
+CB(neighbourunpressed_cb) {
+	return !(tile->flags & TILE_PRESSED);
+}
 CB(neighbourunknown_cb) {
 	return !(tile->flags & (TILE_PRESSED|TILE_FLAGGED));
 }
 CB(neighbourflags_cb) {
 	return !!(tile->flags & TILE_FLAGGED);
 }
+CB(neighbournoflags_cb) {
+	return !(tile->flags & TILE_FLAGGED);
+}
 CB(neighbourneighbour_cb) {
 	return (tile->flags & TILE_PRESSED) && (tile->neighbours > 0);
+}
+CB(neighbourdifference_cb) {
+	int i = 0;
+	int idx = coordstoidx(f, coords);
+	Coordinate **set = (Coordinate **) payload;
+	while (set[i] != NULL) {
+		if (coordstoidx(f, set[i]) == idx) return 0;
+		++i;
+	}
+	return 1;
 }
 #undef CB
 
@@ -75,15 +105,15 @@ ACT(act_singleflagging) {
 	GETTILE(tile);
 	if (!(tile->flags & (TILE_PRESSED|TILE_FLAGGED))) return NULL;
 	if (!tile->neighbours) return NULL;
-	int neighbourunknown = neighbourcount(f, idx, &neighbourunknown_cb, NULL);
+	Coordinate *neighbours[f->maxneighbours];
+	neighbourhood(f, idx, (Coordinate **) neighbours);
+	int neighbourunknown = neighbourcount(f, (Coordinate **) neighbours, &neighbourunknown_cb, NULL);
 	if (!neighbourunknown) return NULL;
-	int neighbourflags = neighbourcount(f, idx, &neighbourflags_cb, NULL);
+	int neighbourflags = neighbourcount(f, (Coordinate **) neighbours, &neighbourflags_cb, NULL);
 	if (tile->neighbours != neighbourunknown + neighbourflags) return NULL;
 
 	Action **ret = malloc(sizeof(Action *)*(neighbourunknown+1));
 	int retidx = 0;
-	Coordinate *neighbours[f->maxneighbours];
-	neighbourhood(f, idx, (Coordinate **) neighbours);
 	int i = 0;
 	while (neighbours[i] != NULL) {
 		Coordinate *c = neighbours[i++];
@@ -105,15 +135,15 @@ ACT(act_safespots) {
 	GETTILE(tile);
 	if (!(tile->flags & (TILE_PRESSED|TILE_FLAGGED))) return NULL;
 	if (!tile->neighbours) return NULL;
-	int neighbourunknown = neighbourcount(f, idx, &neighbourunknown_cb, NULL);
+	Coordinate *neighbours[f->maxneighbours];
+	neighbourhood(f, idx, (Coordinate **) neighbours);
+	int neighbourunknown = neighbourcount(f, (Coordinate **) neighbours, &neighbourunknown_cb, NULL);
 	if (!neighbourunknown) return NULL;
-	int neighbourflags = neighbourcount(f, idx, &neighbourflags_cb, NULL);
+	int neighbourflags = neighbourcount(f, (Coordinate **) neighbours, &neighbourflags_cb, NULL);
 	if (tile->neighbours != neighbourflags) return NULL;
 
 	Action **ret = malloc(sizeof(Action *)*(neighbourunknown+1));
 	int retidx = 0;
-	Coordinate *neighbours[f->maxneighbours];
-	neighbourhood(f, idx, (Coordinate **) neighbours);
 	int i = 0;
 	while (neighbours[i] != NULL) {
 		Coordinate *c = neighbours[i++];
@@ -129,6 +159,80 @@ ACT(act_safespots) {
 	}
 	ret[retidx] = NULL;
 	return ret;
+}
+
+static bool issubset(void **superset, void **subset) {
+	while (*subset != NULL) {
+		void **p = superset;
+		while (*p != NULL) {
+			if (*p == *subset) return 0;
+			++p;
+		}
+		++subset;
+	}
+	return 1;
+}
+
+ACT(act_dualcheck) {
+	GETTILE(a);
+	/* Tiles: a, b
+	 * Indices: idx, bidx
+	 * Coordinates: bcoord
+	 * Neighbourhoods, all: an, bn
+	 * Neighbourhoods, bomb neighbours: anb, bnb
+	 * Neighbourhoods, unpressed: anu, bnu
+	 * HTH
+	 */
+	Coordinate *an[f->maxneighbours];
+	neighbourhood(f, idx, (Coordinate **) an);
+	Coordinate *anu[f->maxneighbours];
+	{ Coordinate **a = an; Coordinate **b = anu; while ((*a++ = *b++)); }
+	neighbourfilter(f, anu, *neighbourunpressed_cb, NULL);
+	{
+		int i = 0;
+		while (an[i] != NULL) {
+			Coordinate *bcoord = an[i++];
+			int bidx = coordstoidx(f, bcoord);
+			Tile *b = &f->tiles[bidx];
+			if (!(b->flags & TILE_PRESSED) || b->neighbours < a->neighbours) continue;
+			/* B has as many as or more bomb neighbours than A */
+
+			Coordinate *bn[f->maxneighbours];
+			neighbourhood(f, bidx, (Coordinate **) bn);
+
+			Coordinate *bnu[f->maxneighbours];
+			{ Coordinate **a = bn; Coordinate **b = bnu; while ((*a++ = *b++)); }
+			neighbourfilter(f, bnu, &neighbourunpressed_cb, NULL);
+
+			if (!issubset((void **) bnu, (void **) anu)) continue;
+			/* A has no bomb neighbours that B doesn't have */
+
+			neighbourfilter(f, bnu, &neighbourdifference_cb, anu);
+
+			if (*bnu == NULL) continue;
+			/* B has unpressed neighbours that A doesn't have */
+			int unflagged = neighbourcount(f, bnu, &neighbournoflags_cb, NULL);
+
+			if (!unflagged) continue;
+
+			int count = 0; while (bnu[count] != NULL); ++count;
+			if (b->neighbours-a->neighbours > count) continue;
+			Action **res = (Action **) malloc(sizeof(Action *)*(count+1));
+			Action act;
+			act.type = (a->neighbours == b->neighbours) ? PRESS : FLAG;
+			int i = 0;
+			while (bnu[i] != NULL) {
+				Coordinate *coord = bnu[i];
+				act.tileidx = coordstoidx(f, coord);
+				res[i] = (Action *) malloc(sizeof(Action));
+				*res[i] = act;
+				i++;
+			}
+			res[i] = NULL;
+			return res;
+		}
+	}
+	return NULL;
 }
 #undef GETTILE
 #undef ACT
